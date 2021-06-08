6e33e1089937d807c111b8c2d7688f4cc4daac1c
Kunj Bhesaniya <bhesaniyakunj5257@gmail.com>
2021-06-08 19:35:52 +0530

    Virtual_Class

---
+++ virtual_class.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+class A
+{
+    public:
+    int a = 10;
+};
+class B : public virtual A
+{};
+class C : public virtual A
+{};
+class D : public B, public C
+{};
+int main()
+{
+    D object;
+    cout<<"a = "<<object.a<<endl;
+
+    return 0;
+}
