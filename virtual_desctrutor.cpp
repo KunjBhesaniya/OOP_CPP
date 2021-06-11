975775e4980b3c38bb8b376949b3e75af405e0e6
Kunj Bhesaniya <bhesaniyakunj5257@gmail.com>
2021-06-11 11:38:07 +0530

    Virtual Destructor

--- virtual_desctrutor.cpp
+++ virtual_desctrutor.cpp
@@ -1,33 +1,33 @@
-#include<iostream>
-using namespace std;
-class Base
-{
-    public:
-    Base()
+    #include<iostream>
+    using namespace std;
+    class Base
     {
-        cout<<"Base Class Constructor"<<endl;
-    }
-    virtual ~Base()
+        public:
+        Base()
+        {
+            cout<<"Base Class Constructor"<<endl;
+        }
+        virtual ~Base()
+        {
+            cout<<"Base Class Desctructor"<<endl;
+        }
+    };
+    class Derived : public Base
     {
-        cout<<"Base Class Desctructor"<<endl;
-    }
-};
-class Derived : public Base
-{
-    public:
-    Derived()
+        public:
+        Derived()
+        {
+            cout<<"Derived Class Constructor"<<endl;
+        }
+        ~Derived()
+        {
+            cout<<"Derived Class Destructor"<<endl;
+        }
+    };
+    int main()
     {
-        cout<<"Derived Class Constructor"<<endl;
-    }
-    ~Derived()
-    {
-        cout<<"Derived Class Destructor"<<endl;
-    }
-};
-int main()
-{
-    Base *bptr = new Derived;
-    delete bptr;
+        Base *bptr = new Derived;
+        delete bptr;
 
-    return 0;
-}
+        return 0;
+    }
